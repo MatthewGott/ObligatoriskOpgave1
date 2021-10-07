c9662f628821de32c1a411c03b422c71814f4e2a
Mathias Gottlieb Oreskov <math523s@edu.easj.dk>
2021-10-07 10:46:39 +0200

    update

--- FootballPlayer.cs
+++ FootballPlayer.cs
@@ -6,45 +6,60 @@ namespace Opgave1Football
     
     public class FootballPlayer
     {
-        public static List<int> IDList = new List<int>();
+        private static List<int> IDList = new List<int>();
 
 
         //Properties of the Class
-        
-        public int ID;
-        public string Name;
-        public int Price;
-        public int ShirtNumber;
+        //ID - Must be unique.
+        public int ID { 
+            get { return ID; }
+            set {
+                if (IDList.Contains(value) == false)
+                {
+                    ID = value;
+                    IDList.Add(value);
+                }
+                else throw new ArgumentOutOfRangeException("ID is already in Use");
+            }
+        }
 
-        //The Constructor for the Class
+        //Name - Must be a minimum of 4 Letters
+        public string Name { 
+            get { return Name; }
+            set {
+                if (value != null && value.Length >= 4) Name = value;
+                else throw new ArgumentOutOfRangeException("Name is too Short, must be a minimum of 4 letters");
+            }
+        }
+
+        //Price - Must be a minimum of 1
+        public int Price { 
+            get { return Price; }
+            set {
+                if (value > 0) Price = value;
+                else throw new ArgumentOutOfRangeException("Invalid Price, must be a Positive number higher than 0");
+            }
+        }
+
+        //ShirtNumber - Must be minimum 1 && maximum 100
+        public int ShirtNumber { 
+            get { return ShirtNumber; }
+            set {
+                if (value > 0 && value < 101) ShirtNumber = value;
+                else throw new ArgumentOutOfRangeException("Invalid ShirtNumber, must be higher than 0 AND lower than 101");
+            }
+        }
+
+        //Primary Constructor
         public FootballPlayer(int id, string name, int price, int shirtnumber)
         {
-            //Sets the ID for the Player
-            if (IDList.Contains(id) == false) {
-                ID = id;
-                IDList.Add(id);
-                }
-            else throw new ArgumentOutOfRangeException("ID is already in Use");
-
-            //Checks Name length and applies if valid, throws an exception if invalid
-            if (name.Length >= 4) Name = name;
-            else throw new ArgumentOutOfRangeException("Name is too Short, must be a minimum of 4 letters");
-
-            //Checks if Price is MORE than 0, throws an exception if invalid
-            if (price > 0) Price = price;
-            else throw new ArgumentOutOfRangeException("Invalid Price submitted, must be a Positive number (higher than 0)");
-
-            //Checks if Shirtnumber is Higher than 0 AND lower than 101, throws an exception if invalid
-            if (shirtnumber > 0 && shirtnumber < 101) ShirtNumber = shirtnumber;
-            else throw new ArgumentOutOfRangeException("Invalid ShirtNumber submitted, must be higher than 0 AND lower than 101");
+            ID = id;
+            Name = name;
+            Price = price;
+            ShirtNumber = shirtnumber;
         }
 
-        //Empty Constructor - Used for JSON
+        //Empty Constructor
         public FootballPlayer() { }
-
-        public override string ToString()
-        {
-            return ("ID : " + ID.ToString() + ", Name : " + Name + ", Price : " + Price.ToString() + ", ShirtNumber : " + ShirtNumber.ToString());
-        }
     }
 }
